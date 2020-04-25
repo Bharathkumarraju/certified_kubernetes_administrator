d6853e882d817db6d8829dc5c0809dbfeea1f2b4
Bharath <bharath@hooq.tv>
2020-04-25 14:56:40 +0800

    CKA exam link

--- cka_exam.sh
+++ cka_exam.sh
@@ -9,3 +9,6 @@ Exam Tips: http://training.linuxfoundation.org/go//Important-Tips-CKA-CKAD
 
 
 Use the code - DEVOPS15 - while registering for the CKA or CKAD exams at Linux Foundation to get a 15% discount.
+
+slack: https://app.slack.com/client/TDSBA9B9V/CDR9R5QRG/details/members
+
