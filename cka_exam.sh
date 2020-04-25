feafea93b406c4b269deb8f3225d57248753cd97
Bharath <bharath@hooq.tv>
2020-04-25 14:45:57 +0800

    details about the exam

---
+++ cka_exam.sh
@@ -0,0 +1,11 @@
+Certified Kubernetes Administrator: https://www.cncf.io/certification/cka/
+
+Exam Curriculum (Topics): https://github.com/cncf/curriculum
+
+Candidate Handbook: https://www.cncf.io/certification/candidate-handbook
+
+Exam Tips: http://training.linuxfoundation.org/go//Important-Tips-CKA-CKAD
+
+
+
+Use the code - DEVOPS15 - while registering for the CKA or CKAD exams at Linux Foundation to get a 15% discount.
