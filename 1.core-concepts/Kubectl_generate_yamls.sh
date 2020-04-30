b62b6f1dc1f0b342229dd4de2e49fd75998ce6e6
Bharath <bharath@hooq.tv>
2020-04-30 08:45:38 +0800

    kubectl run nginx --image=nginx --generator=run-pod/v1 --dry-run -o yaml kubectl create deployment nginx --image=nginx --dry-run -o yaml

---
+++ 1.core-concepts/Kubectl_generate_yamls.sh
@@ -0,0 +1,20 @@
+Create an NGINX Pod
+------------------------->
+
+kubectl run --generator=run-pod/v1 nginx --image=nginx
+
+Generate POD Manifest YAML file (-o yaml). Do not create it(--dry-run)
+kubectl run --generator=run-pod/v1 nginx --image=nginx --dry-run -o yaml
+
+Create a deployment
+------------------------>
+kubectl create deployment --image=nginx nginx
+
+Generate Deployment YAML file (-o yaml). Do not create it(--dry-run)
+kubectl create deployment --image=nginx nginx --dry-run -o yaml
+
+
+Generate Deployment YAML file (-o yaml). Do not create it(--dry-run) with 4 Replicas (--replicas=4)
+kubectl create deployment --image=nginx nginx --dry-run -o yaml > nginx-deployment.yaml
+
+Save it to a file, make necessary changes to the file (for example, adding more replicas) and then create the deployment.
---
+++ 1.core-concepts/kubectl_dryrun_get_yamls.sh
@@ -0,0 +1,46 @@
+bharathdasaraju@MacBook-Pro external $ kubectl run nginx --image=nginx --generator=run-pod/v1 --dry-run -o yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  creationTimestamp: null
+  labels:
+    run: nginx
+  name: nginx
+spec:
+  containers:
+  - image: nginx
+    name: nginx
+    resources: {}
+  dnsPolicy: ClusterFirst
+  restartPolicy: Always
+status: {}
+bharathdasaraju@MacBook-Pro external $
+
+
+
+bharathdasaraju@MacBook-Pro external $ kubectl create deployment nginx --image=nginx --dry-run -o yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  creationTimestamp: null
+  labels:
+    app: nginx
+  name: nginx
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: nginx
+  strategy: {}
+  template:
+    metadata:
+      creationTimestamp: null
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - image: nginx
+        name: nginx
+        resources: {}
+status: {}
+bharathdasaraju@MacBook-Pro external $
