a6ded9f2a017e477261a343588002135c4787a61
Bharath <bharath@hooq.tv>
2020-05-04 05:08:42 +0800

    Labels in Pod, replicaSet,Service and Annotations in the replicaSet

---
+++ 2.Scheduling/2.Labels_and_Selectors/Annotations.sh
@@ -0,0 +1,8 @@
+While labels and selectors are used to group and select objects...
+
+The Annotations are used to record other details for inflammatory purpose.
+For example:
+-------------->
+Tool details like name, version build information(or)
+contact details, phonenumbers, emails etc.. that may be used for some kind of integration purpose.
+
