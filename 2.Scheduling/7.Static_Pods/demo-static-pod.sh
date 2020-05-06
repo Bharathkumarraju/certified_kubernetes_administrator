88b3a7196c4bcc2c7618f5c5fb2a3325e7c4ca1d
Bharath <bharath@hooq.tv>
2020-05-06 10:49:17 +0800

    All ablout static pods

--- 2.Scheduling/7.Static_Pods/demo-static-pod.sh
+++ 2.Scheduling/7.Static_Pods/demo-static-pod.sh
@@ -3,3 +3,23 @@ Q: Create a static pod named static-busybox that uses the busybox image and the 
 master $ kubectl run --restart=Never --image=busybox static-busybox --dry-run -o yaml --command -- sleep 1000 > /etc/kubernetes/manifests/static-busybox.yaml
 master $
 
+bharathdasaraju@MacBook-Pro ~ $ kubectl run --restart=Never --image=busybox static-busy-box --dry-run -o yaml --command -- sleep 1000
+apiVersion: v1
+kind: Pod
+metadata:
+  creationTimestamp: null
+  labels:
+    run: static-busy-box
+  name: static-busy-box
+spec:
+  containers:
+  - command:
+    - sleep
+    - "1000"
+    image: busybox
+    name: static-busy-box
+    resources: {}
+  dnsPolicy: ClusterFirst
+  restartPolicy: Never
+status: {}
+bharathdasaraju@MacBook-Pro ~ $
