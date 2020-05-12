27cd1cacc891d1cdfb042780673c271b24c9a78e
Bharath <bharath@hooq.tv>
2020-05-12 08:20:18 +0800

    kubernetes software versions 1.Major 2.Minor 3.Patch 4.alpha 5.beta

---
+++ 5.Cluster Maintenance/2.kubernetes_software_versions/intro.sh
@@ -0,0 +1,22 @@
+v1.18.0
+
+bharathdasaraju@MacBook-Pro ~ $ kubectl get nodes
+NAME       STATUS   ROLES    AGE   VERSION
+minikube   Ready    master   43d   v1.18.0
+bharathdasaraju@MacBook-Pro ~ $
+
+v1.18.0 it has 3 parts
+
+1. MAJOR Version: v1
+2. MINOR Version: 18
+3. PATCH Version: 0
+Apart from these we have also see alpha and beta releases.
+
+First all the bug fixes and improvements first go into alpha release tag alpha in the release v1.18.1-alpha
+In the alpha release v1.18.1-alpha the features are disabled by default may be buggy.
+
+Then for there they make their way to beta release i.e. v1.18.1-beta where the code is well tested.
+and new features are enabled by default and finally they make their way to final stable release i.e. v1.18.1
+
+
+
