49e045db38a26458ec61e6f21b4bcf53d602700b
Bharath <bharath@hooq.tv>
2020-05-16 08:49:12 +0800

    All about security in kubernetes cluster 1. TLS Certs 2. K8s CLuster AUthentication 3. K8s Cluster Authorization using RBAC roles and rolebindings

---
+++ 6.Security/intro.sh
@@ -0,0 +1,17 @@
+How does someone gain access to the kubenetes cluster
+and how are their actions controlled etc.. at higher level.
+
+What are the various authentication mechanisms available to access the kubenetes cluster.
+
+TLS Certificates for the cluster components
+
+Secure Persistent Key Value store
+
+Authorization
+--> RBAC (Role Based Access Controls)
+
+Image security
+
+Security Contexts
+
+Network Policies
