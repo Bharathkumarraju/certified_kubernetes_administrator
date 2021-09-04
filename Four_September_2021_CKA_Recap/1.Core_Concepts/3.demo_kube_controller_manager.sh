d5839d21a3e7972efbc947e565dccf0092edf466
bharath <bharathkumar.dasaraju@singtel.com>
2021-09-04 16:08:40 +0800

    All about controllers in kubernetes

---
+++ Four_September_2021_CKA_Recap/1.Core_Concepts/3.demo_kube_controller_manager.sh
@@ -0,0 +1,11 @@
+1. Node Controller
+2. Replica Controller
+3. Deployment Controller
+4. Namespace Controller
+5. Endpoint Controller
+6. Job Controller
+7. Service-Account-Controller
+8. Stateful-Set 
+9. PV-Binder-Controller
+10. PV-Protection-Controller 
+
