14964fcbf3077bd0d06b12303dee0a595f39470d
bharath <bharathkumar.dasaraju@singtel.com>
2021-09-13 07:20:40 +0800

    :mag: all about k8s service types NodePort ClusterIP Loadbalancer and externalName :mag:

--- Four_September_2021_CKA_Recap/1.Core_Concepts/19.demo_service_NodePort.sh
+++ Four_September_2021_CKA_Recap/1.Core_Concepts/19.demo_service_NodePort.sh
@@ -10,7 +10,7 @@ spec:
   ports:
    - targetPort: 80 # Pod listens on this port, if we do not specify this value it assumes same as port.
      port: 80  # Service Port this is the only mandatory files
-     nodePort: 30008  # Node listens on this port...can access using node IP-Address
+     nodePort: 30008  # Node listens on this port...can access using node IP-Address this is also optional
   selector:
     app: bkapp
     type: front-end
--- Four_September_2021_CKA_Recap/1.Core_Concepts/20.demo_service_clusterIP.sh
+++ Four_September_2021_CKA_Recap/1.Core_Concepts/20.demo_service_clusterIP.sh
@@ -1,1 +1,10 @@
 # ClusterIP service type
+
+1. front-end pods
+2. back-end pods
+3. redis pods
+
+front-end pods need to connect to back-end pods for internal communication
+And back-end pods needs to communicate to redis pods for internal configuration
+
+
