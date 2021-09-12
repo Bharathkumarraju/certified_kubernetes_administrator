5e68f58ff8f0155cc3d928d53eeb3a2b59c77988
bharath <bharathkumar.dasaraju@singtel.com>
2021-09-13 07:54:37 +0800

    :goal_net: all service types NodePort, ClusterIP and LoadBalancer :goal_net:

--- Four_September_2021_CKA_Recap/1.Core_Concepts/20.demo_service_clusterIP.sh
+++ Four_September_2021_CKA_Recap/1.Core_Concepts/20.demo_service_clusterIP.sh
@@ -8,3 +8,31 @@ front-end pods need to connect to back-end pods for internal communication
 And back-end pods needs to communicate to redis pods for internal configuration
 
 
+
+apiVersion: v1 
+kind: Service
+metadata:
+  name: back-end 
+spec:
+  type: ClusterIP
+  ports:
+   - targetPort : 80
+     port: 80
+  selector:
+    app: bkapp2
+    type: back-end
+
+
+bharathdasaraju@MacBook-Pro 1.Core_Concepts (master) $ kubectl run redis --image=redis --dry-run=client -o yaml > bkweb_pod2.yaml
+bharathdasaraju@MacBook-Pro 1.Core_Concepts (master) $ kubectl create -f bkweb_pod2.yaml
+pod/redis created
+bharathdasaraju@MacBook-Pro 1.Core_Concepts (master) $ kubectl create -f service_ClusterIP.yaml 
+service/back-end created
+bharathdasaraju@MacBook-Pro 1.Core_Concepts (master) $ kubectl get svc
+NAME            TYPE        CLUSTER-IP      EXTERNAL-IP   PORT(S)        AGE
+back-end        ClusterIP   10.102.211.63   <none>        80/TCP         7s
+bkapp-service   NodePort    10.102.41.98    <none>        80:30009/TCP   45h
+kubernetes      ClusterIP   10.96.0.1       <none>        443/TCP        8d
+bharathdasaraju@MacBook-Pro 1.Core_Concepts (master) $
+
+
