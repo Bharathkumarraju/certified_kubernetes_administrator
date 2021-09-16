cffb1f46549e0822d7993ecfa23532941bc0f1b3
bharath <bharathkumar.dasaraju@singtel.com>
2021-09-16 10:09:26 +0800

    :alien: add nodeSelectors to pod specification file :alien:

---
+++ Four_September_2021_CKA_Recap/2.Scheduling/10.demo_node_selectors.sh
@@ -0,0 +1,12 @@
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    run: nginx
+  name: nginx
+spec:
+  containers:
+  - image: nginx
+    name: nginx
+  nodeSelector:
+    size: Large  # This is the node Label
