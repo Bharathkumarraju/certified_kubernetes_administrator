9eb6d3149154aebea45bce77c4ec9d8e79cbe6bd
Bharath Dasaraju <bharathdasaraju@MacBook-Pro.local>
2021-09-18 06:02:01 +0800

    :see_no_evil: node affinity with logical conditions :see_no_evil:

--- Four_September_2021_CKA_Recap/2.Scheduling/11.demo_node_affnity.sh
+++ Four_September_2021_CKA_Recap/2.Scheduling/11.demo_node_affnity.sh
@@ -1,3 +1,6 @@
 We can not provide advanced options like Large (or) Medium (or) Not Small like that with NodeSeletors
 
-So Node Affinity gives us the advanced capability to place a pod on particular Node
+So Node Affinity gives us the advanced capability to place a pod on particular Node with conditions.
+
+
+
