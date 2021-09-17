f2e354198e481b031d66d3e806a19aedf574f04b
bharath <bharathkumar.dasaraju@singtel.com>
2021-09-17 12:53:26 +0800

    :wrench: NodeAffinity gives nice ability to place a pod on specific node :wrench:

---
+++ Four_September_2021_CKA_Recap/2.Scheduling/11.demo_node_affnity.sh
@@ -0,0 +1,3 @@
+We can not provide advanced options like Large (or) Medium (or) Not Small like that with NodeSeletors
+
+So Node Affinity gives us the advanced capability to place a pod on particular Node
