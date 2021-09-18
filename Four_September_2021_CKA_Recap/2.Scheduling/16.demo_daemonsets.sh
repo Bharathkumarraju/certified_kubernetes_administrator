fb48e284961d3b55a03822e1bae2c3ce9c15a4ab
Bharath <bhrth.dsra1@gmail.com>
2021-09-18 11:37:26 +0800

    :sparkles: deploy monitoring agent or log collector as daemonset in k8s cluster :sparkles:

---
+++ Four_September_2021_CKA_Recap/2.Scheduling/16.demo_daemonsets.sh
@@ -0,0 +1,11 @@
+daemonset runs one copy of pod on each node in the cluster
+
+Whenever a new node joins a cluster a replica of pod automatically added to that node.
+
+
+Examples like 
+1.deploying a monitoring agent and 
+2.log collector in each node
+
+
+
