476652acd21081d887cd6034fb38880ac81514db
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2021-09-22 07:47:05 +0800

    :sparkles: all about security in k8s :sparkles:

---
+++ Four_September_2021_CKA_Recap/6.Security/1.demo_concepts.sh
@@ -0,0 +1,11 @@
+1. Security Primitives
+2. Authentication
+3. TLS
+4. Certificates API
+5. KubeConfig
+6. API Groups
+7. Authorization
+8. RBAC
+9. CLuster Roles
+10. Image Security
+11. Network Policies
