c8807a2bd9f5847ca8c1b4ec6b2539dfdfd31107
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2021-09-23 11:46:17 +0800

    :beers: kubectl config use-context research :beers:

---
+++ Four_September_2021_CKA_Recap/6.Security/12.demo_kubeconfig_labs.sh
@@ -0,0 +1,95 @@
+root@controlplane:~# kubectl config view
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: DATA+OMITTED
+    server: https://controlplane:6443
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: kubernetes-admin
+  name: kubernetes-admin@kubernetes
+current-context: kubernetes-admin@kubernetes
+kind: Config
+preferences: {}
+users:
+- name: kubernetes-admin
+  user:
+    client-certificate-data: REDACTED
+    client-key-data: REDACTED
+root@controlplane:~# 
+
+
+----------------------------------------------------------------------------------------->
+
+
+root@controlplane:~# kubectl config view --kubeconfig=my-kube-config 
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority: /etc/kubernetes/pki/ca.crt
+    server: https://controlplane:6443
+  name: development
+- cluster:
+    certificate-authority: /etc/kubernetes/pki/ca.crt
+    server: https://controlplane:6443
+  name: kubernetes-on-aws
+- cluster:
+    certificate-authority: /etc/kubernetes/pki/ca.crt
+    server: https://controlplane:6443
+  name: production
+- cluster:
+    certificate-authority: /etc/kubernetes/pki/ca.crt
+    server: https://controlplane:6443
+  name: test-cluster-1
+contexts:
+- context:
+    cluster: kubernetes-on-aws
+    user: aws-user
+  name: aws-user@kubernetes-on-aws
+- context:
+    cluster: test-cluster-1
+    user: dev-user
+  name: research
+- context:
+    cluster: development
+    user: test-user
+  name: test-user@development
+- context:
+    cluster: production
+    user: test-user
+  name: test-user@production
+current-context: test-user@development
+kind: Config
+preferences: {}
+users:
+- name: aws-user
+  user:
+    client-certificate: /etc/kubernetes/pki/users/aws-user/aws-user.crt
+    client-key: /etc/kubernetes/pki/users/aws-user/aws-user.key
+- name: dev-user
+  user:
+    client-certificate: /etc/kubernetes/pki/users/dev-user/developer-user.crt
+    client-key: /etc/kubernetes/pki/users/dev-user/dev-user.key
+- name: test-user
+  user:
+    client-certificate: /etc/kubernetes/pki/users/test-user/test-user.crt
+    client-key: /etc/kubernetes/pki/users/test-user/test-user.key
+root@controlplane:~# 
+
+
+
+root@controlplane:~# kubectl config use-context research --kubeconfig=my-kube-config 
+Switched to context "research".
+root@controlplane:~# 
+
+root@controlplane:~# kubectl config current-context --kubeconfig=my-kube-config
+research
+root@controlplane:~# 
+
+
+root@controlplane:/etc/kubernetes/pki/users/dev-user# kubectl config current-context 
+research
+root@controlplane:/etc/kubernetes/pki/users/dev-user#
+
