6f1c48fe088a9b4796229e0e5e781cdc15e3a1a2
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2021-09-23 15:54:20 +0800

    :green_heart: Roles and Rolebindings falls under the scope of namespace :green_heart:

--- Four_September_2021_CKA_Recap/6.Security/15.demo_Role_Based_Access_Controls(RBAC).sh
+++ Four_September_2021_CKA_Recap/6.Security/15.demo_Role_Based_Access_Controls(RBAC).sh
@@ -1,6 +1,7 @@
 RoleBasedAccesControl(RBAC)
 -------------------------------->
 How to Create a role 
+==============================================>
 
 MacBook-Pro:6.Security bharathdasaraju$ kubectl create role developer --verb=get --verb=list --verb=watch --resource=pods --dry-run=client -o yaml > deveoper-role.yaml
 MacBook-Pro:6.Security bharathdasaraju$ 
@@ -19,3 +20,63 @@ PolicyRule:
   ---------  -----------------  --------------  -----
   pods       []                 []              [get list watch create update delete]
 MacBook-Pro:6.Security bharathdasaraju$ 
+
+
+MacBook-Pro:6.Security bharathdasaraju$ kubectl apply -f deveoper-role.yaml 
+role.rbac.authorization.k8s.io/developer configured
+MacBook-Pro:6.Security bharathdasaraju$ kubectl describe role developer
+Name:         developer
+Labels:       <none>
+Annotations:  <none>
+PolicyRule:
+  Resources  Non-Resource URLs  Resource Names  Verbs
+  ---------  -----------------  --------------  -----
+  ConfigMaP  []                 []              [create delete]
+  pods       []                 []              [get list watch create update delete]
+MacBook-Pro:6.Security bharathdasaraju$ 
+
+
+Create a RoleBinding:
+==============================================>
+
+RoleBinding is to assign that created to role to specific user/group
+
+MacBook-Pro:6.Security bharathdasaraju$  kubectl create rolebinding devuser-developer-binding --role=developer --user=dev-user --dry-run=client -o yaml
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: devuser-developer-binding
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: Role
+  name: developer
+subjects:
+- apiGroup: rbac.authorization.k8s.io
+  kind: User
+  name: dev-user
+MacBook-Pro:6.Security bharathdasaraju$
+
+MacBook-Pro:6.Security bharathdasaraju$  kubectl create rolebinding devuser-developer-binding --role=developer --user=dev-user --dry-run=client -o yaml > devuser-developer-binding.yaml
+MacBook-Pro:6.Security bharathdasaraju$ 
+
+MacBook-Pro:6.Security bharathdasaraju$ kubectl apply -f devuser-developer-binding.yaml
+rolebinding.rbac.authorization.k8s.io/devuser-developer-binding created
+MacBook-Pro:6.Security bharathdasaraju$ kubectl describe rolebinding devuser-developer-binding
+Name:         devuser-developer-binding
+Labels:       <none>
+Annotations:  <none>
+Role:
+  Kind:  Role
+  Name:  developer
+Subjects:
+  Kind  Name      Namespace
+  ----  ----      ---------
+  User  dev-user  
+MacBook-Pro:6.Security bharathdasaraju$ 
+
+note that roles and rolebindings falls under the scope of the namespaces.
+
+where as 
+
+clusterrole and clusterrolebinding falls under the scope of the cluster.
+
