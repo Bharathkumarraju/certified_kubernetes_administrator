5e4cbac6d88289fb8219adc52377f6001f59ef83
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2021-09-26 06:31:04 +0800

    :rotating_light: get started with networking in kubernetes :rotating_light:

---
+++ Four_September_2021_CKA_Recap/8.Networking/1.demo_networing_concepts.sh
@@ -0,0 +1,12 @@
+1. Configuring interfaces and IPAddresses.
+2. Configuring gateways and routes.
+3. Basics of Name Resolution
+4. DNS configuration on Linux systems
+5. How to get started with core-dns 
+6. how docker uses network namespaces inernally
+7. CLuster networking needs
+8. Pod networking concepts
+9. CNI --> Container Network Interface
+10. ClusterDNS
+11. Ingress networking
+
