68b3e02aaac061899c9ca5b7ffd30b536e80b1a1
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2021-09-27 15:03:46 +0800

    :rotating_light: kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"  :rotating_light:

--- Four_September_2021_CKA_Recap/8.Networking/11.CNI_Weave.sh
+++ Four_September_2021_CKA_Recap/8.Networking/11.CNI_Weave.sh
@@ -17,3 +17,10 @@ role.rbac.authorization.k8s.io/weave-net created
 rolebinding.rbac.authorization.k8s.io/weave-net created
 daemonset.apps/weave-net created
 MacBook-Pro:8.Networking bharathdasaraju$ 
+
+MacBook-Pro:8.Networking bharathdasaraju$ kubectl get all -n kube-system | grep -i weave
+pod/weave-net-7p8fq                    2/2     Running   1          2m46s
+daemonset.apps/weave-net    1         1         1       1            1           <none>                   2m46s
+MacBook-Pro:8.Networking bharathdasaraju$ 
+
+
