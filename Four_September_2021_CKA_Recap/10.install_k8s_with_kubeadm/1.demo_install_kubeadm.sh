4947ad1ed10edb7aae8093daaf72caf6d2347e3b
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2021-09-29 05:50:23 +0800

    :lock: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/ :lock:

---
+++ Four_September_2021_CKA_Recap/10.install_k8s_with_kubeadm/1.demo_install_kubeadm.sh
@@ -0,0 +1,16 @@
+MacBook-Pro:certified-kubernetes-administrator-course bharathdasaraju$ vagrant status
+Current machine states:
+
+kubemaster                not created (virtualbox)
+kubenode01                not created (virtualbox)
+kubenode02                not created (virtualbox)
+
+This environment represents multiple VMs. The VMs are all listed
+above with their current state. For more information about a specific
+VM, run `vagrant status NAME`.
+MacBook-Pro:certified-kubernetes-administrator-course bharathdasaraju$ 
+
+
+
+https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+
