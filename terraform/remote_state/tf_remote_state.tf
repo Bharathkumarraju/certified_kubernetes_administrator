d619377190085b4a888e9f3ab4d00901ca659060
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2021-10-03 14:50:05 +0800

    :tada: lets use default terraform version from github VM ubuntu-latest :tada:

--- terraform/remote_state/tf_remote_state.tf
+++ terraform/remote_state/tf_remote_state.tf
@@ -33,7 +33,7 @@ resource "aws_dynamodb_table" "bharaths-terraform-locks" {
 }
 
 output "bharath_s3_backend_arn" {
-  value = aws_s3_bucket.bharath_tf_state_store.arn
+  value       = aws_s3_bucket.bharath_tf_state_store.arn
   description = "The S3 Bucket ARN"
 }
 
@@ -41,4 +41,3 @@ output "bharath_dynamodb_table_name" {
   value = aws_dynamodb_table.bharaths-terraform-locks.name
   description = "The Name of the Dynamo DB table"
 }
-
